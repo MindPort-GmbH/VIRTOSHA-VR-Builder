73c753e9148b34dd45d28b26acec7b62c0a387b2
Marcello Tridenti <marcello.tridenti@tutanota.com>
2024-07-29 17:38:25 +0200

    Tests basics

---
+++ Tests/DrillHolesConditionTests.cs
@@ -0,0 +1,15 @@
+namespace VRBuilder.VIRTOSHA.Tests.Conditions
+{
+    public class DrillHolesConditionTests : RuntimeTests
+    {
+        [UnityTest]
+        private IEnumerator DrillHolesConditionIsFulfilled()
+        {
+            // Given a drill holes condition,
+
+            // When the specified holes are drilled,
+
+            // Then it is fulfilled as intended.
+        }
+    }
+}
