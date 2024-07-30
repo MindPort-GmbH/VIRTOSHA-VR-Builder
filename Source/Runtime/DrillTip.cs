59d0485ee6bd16124166692f556ea9801bc4c297
Marcello Tridenti <marcello.tridenti@tutanota.com>
2024-07-30 11:18:51 +0200

    docs

--- Source/Runtime/DrillTip.cs
+++ Source/Runtime/DrillTip.cs
@@ -3,6 +3,9 @@ using VRBuilder.VIRTOSHA.Properties;
 
 namespace VRBuilder.VIRTOSHA
 {
+    /// <summary>
+    /// Component representing the tip of the drill, used to register collisions when starting to drill holes.
+    /// </summary>
     public class DrillTip : MonoBehaviour
     {
         private DrillBit parentDrillBit;
@@ -10,6 +13,11 @@ namespace VRBuilder.VIRTOSHA
         public void Init(DrillBit parent)
         {
             parentDrillBit = parent;
+
+            if (GetComponent<Collider>() == null)
+            {
+                Debug.LogError($"No collider is set on {typeof(DrillTip).Name} on '{gameObject.name}'. A collider is needed for the component to work as intended. Please add one.");
+            }
         }
 
         private void OnTriggerEnter(Collider other)
